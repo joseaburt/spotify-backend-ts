f58f67ba15133eb5c2e5677e551bb17977959347
JoseAburto <jose.aburto@delinernet.com>
2023-07-07 16:29:12 +0200

    feat(domain/shared): adds security types

---
+++ src/domain/shared/security.ts
@@ -0,0 +1,14 @@
+export type AccessToken = string;
+
+export type WithPassword<T> = T & { password: string };
+
+export type WithToken<T> = T & { token: string };
+
+export type WithoutPassword<T extends WithPassword<any>> = Omit<T, 'password'>;
+
+export interface SecurityUtilsService {
+  createToken(payload: any): Promise<AccessToken>;
+  decodeToken<T = any>(token: AccessToken): Promise<T>;
+  encodePassword(password: string): Promise<string>;
+  arePasswordTheSame(rawPassword: string, hashedPassword: string): Promise<boolean>;
+}
