48c71ff61a95435373762140b312f3b26c612f47
JoseAburto <jose.aburto@delinernet.com>
2023-07-09 17:29:12 +0200

    feat(infrastructure/security): adds jwt security tools

---
+++ src/infrastructure/security/jwt.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+const { JWT_SECRET } = process.env;
+
+export default class JWT {
+  private secret: string;
+  private static instance: JWT;
+
+  private constructor() {
+    if (!JWT_SECRET) throw new Error('JWTNoSecretValueDefined');
+    this.secret = JWT_SECRET;
+  }
+
+  public sign(payload: any) {
+    return jwt.sign(payload, this.secret);
+  }
+
+  public verify<T>(token: string): T {
+    return jwt.verify(token, this.secret) as T;
+  }
+
+  public static getInstance(): JWT {
+    if (!this.instance) this.instance = new JWT();
+    return this.instance;
+  }
+}
