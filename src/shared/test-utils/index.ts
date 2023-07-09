0a3acc83262916490edb7288c21f68325b3f7043
JoseAburto <jose.aburto@delinernet.com>
2023-07-09 23:30:29 +0200

    ref(shared): exports test utils

--- src/shared/test-utils/index.ts
+++ src/shared/test-utils/index.ts
@@ -1,7 +1,21 @@
 import { User } from '../../domain/users/index.model';
+import DataSource from '@joseaburt/mysql2-query-builder';
 import Encrypt from '../../infrastructure/security/encrypt';
+
+export const testConnection = DataSource.createDataSource({
+  port: 3306,
+  password: '',
+  user: 'root',
+  host: 'localhost',
+  database: 'spotify_clone',
+});
 
 export function createDbUser(rawPassword: string): Pick<User, 'id' | 'email' | 'password'> {
   const hashedPassword = Encrypt.encode(rawPassword);
   return { id: 1, password: hashedPassword, email: 'email@gmail.com' };
 }
+
+export const jwtServiceMock = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
